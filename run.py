73e269aa0070168b13ee0edaa0d13cdf99a515b0
Luiz Smania <luizsmania1@gmail.com>
2023-09-14 19:20:46 +0000

    code remade in an easier way, add input to user speciy  needs

--- run.py
+++ run.py
@@ -1,41 +1,29 @@
 import random
 import string
 
-def generate_password(min_length, numbers=True, special_characters=True):
-    letters = string.ascii_letters
-    digits = string.digits
-    special = string.punctuation
-    
-    characters = letters
-    if numbers:
-        characters += digits
-    if special_characters:
-        characters += special
-        
+def generate_password(length, has_letters=True, has_symbols=True):
     pwd = ""
-    meets_criteria = False
-    has_number = False
-    has_special = False
-    
-    while not meets_criteria or len(pwd) < min_length:
-        new_char = random.choice(characters)
-        pwd += new_char
-        
-        if new_char in digits:
-            has_number = True  # Corrected variable name
-        elif new_char in special:
-            has_special = True
-            
-        meets_criteria = True
-        if numbers:
-            meets_criteria = has_number
-        if special_characters:
-            meets_criteria = meets_criteria and has_special
+    letters = string.ascii_letters
+    numbers = string.digits
+    symbols = string.punctuation
 
-        
-        
-    
+    while len(pwd) < length:
+        new_letter = random.choice(letters)
+        new_number = random.choice(numbers)
+        new_symbol = random.choice(symbols)
+
+        choice = random.randint(0,3)
+        if choice == 1 and has_letters:
+            pwd += new_letter
+        elif choice == 2:
+            pwd += new_number
+        elif choice == 3 and has_symbols:
+            pwd += new_symbol
+                
     return pwd
-        
-pwd = generate_password(10)
+
+what_length = int(input("Enter the minimum length: "))
+want_number = input("Do you want to have numbers (y/n)?").lower() == "y"
+want_special = input("Do you want to have special characters (y/n)?").lower() == "y"
+pwd = generate_password(what_length, want_number, want_special)
 print(pwd)
